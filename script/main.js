770839f963deb80341367d0fc5a3da51d7039b06
Andrii Nyzhnyk <andrij1510@gmail.com>
2017-07-26 19:00:44 +0300

    prototype#1

---
+++ script/main.js
@@ -0,0 +1,22 @@
+var module = angular.module("MyApp", []);
+
+
+
+function validInput(input) {
+    console.log(input);
+    var value = input.value;
+    var rep = /[-\.;":'a-zA-Zа-яА-Я]/;
+
+    if (rep.test(value)) {
+        value = value.replace(rep, '');
+        input.value = value;
+    }
+    
+    if(+input.value.length > 19) {
+        console.log(+input.value.length);
+        input.value = value.substring(0, value.length - 1)
+    }
+
+
+}
+
